1e237edaf412e123e2f520433ddea4b838947370
Keith Crawford <keithacrawfordjr@gmail.com>
2023-08-25 16:38:39 -0500

    removed changes

--- script.js
+++ script.js
@@ -14,22 +14,6 @@ for (var i=1; i<=5; i++) {
             currentImage.alt= this.alt;
         }
     );
-    /** a set of if else if statements to help determine the image alt description */
-    if (i=1) {
-        thumb.alt= "A blue rose";
-    }
-    else if (i=2) {
-        thumb.alt= "A heart made of butterflys"
-    }
-    else if (i=3) {
-        thumb.alt= "Flowers and hearts"
-    }
-    else if (i=4) {
-        thumb.alt= "A pink origami rose"
-    }
-    else if (i=5) {
-        thumb.alt= "A purple origami flower"
-    }
 }
 
 
