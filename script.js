ff7daef2c61dd42d185060a687a9f6d900ad99f7
Keith Crawford <keithacrawfordjr@gmail.com>
2023-11-24 16:48:02 -0600

    Fixed up the mobile version of the thumb gallery

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 /** getting variables from HTML */
 
-var imageThumbs=document.getElementById("thumbs");
-var currentImage= document.getElementById("current-img");
+var imageThumbs=document.querySelector("#thumbs");
+var currentImage= document.querySelector("#current-img");
+let imageAlt= ['blue flower', 'butterfly hearts', 'rose and hearts', 'pink origami flower',
+'purple origami flower', 'farewell card', 'christmas card', 'butterfly', 'a rose', 'a cactus', 
+'birthday card', 'inside birthday card'];
 
+/**Creating thumbnail gallery */
 for (var i=1; i<=12; i++) {
     var thumb= document.createElement("img");
-    thumb.src= "images/image" + i + ".jpg"
+    thumb.src= "images/image" + i + ".jpg";
+    thumb.alt= imageAlt[i];
     imageThumbs.appendChild(thumb);
     thumb.classList.add("thumb")
     thumb.addEventListener(
