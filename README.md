95bfded07a690055cecc03330438c44d8ab24422
crawfordk99 <keithacrawfordjr@gmail.com>
2026-07-12 22:03:48 -0500

    Fixed css form call, and updated read me

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ I created this site for my sister, and to also get some practice using HTML/CSS/
 
 Originally, I didn't protect her email, but now I have added webflow services to help hide her email from spam bots. 
 
+## Tech Stack
+
+-HTML/CSS/JavaScript
+-[webflow](https://app.web3forms.com)
+
 
--- script.js
+++ script.js
@@ -23,6 +23,7 @@ for (var i=1; i<=12; i++) {
 
 // Updated form handling code to ensure it works correctly with the form element and submit button
 // , including safety checks and proper handling of input/button types.
+// Ensures also that the page is loaded with all the elements before attaching event listeners.
 
 document.addEventListener('DOMContentLoaded', () => {
     // 1. Find the element with the ID
